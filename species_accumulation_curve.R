83a8ea969c813e91bd6b200c701caad38b1e6d1b
cabanelas <alexcabanelas_14@hotmail.com>
2025-07-22 15:30:01 -0400

    new curve with taxa grouped

--- species_accumulation_curve.R
+++ species_accumulation_curve.R
@@ -18,6 +18,7 @@
 #### ------------------------------------------ #####
 library(tidyverse)
 library(vegan)
+library(taxize)
 
 #### ------------------------------------------ #####
 #            Data -----
@@ -260,3 +261,183 @@ library(knitr)
 
 kable(sample_table, format = "markdown")
 
+#### ------------------------------------------ #####
+#      Grouping prey items to broader level -----
+#### ------------------------------------------ #####
+taxa <- colnames(presence_matrix)
+
+# get GBIF IDs; suppress messages for clarity
+gbif_ids <- get_gbifid(taxa, messages = FALSE, ask = FALSE)
+gbif_class <- classification(gbif_ids, db = "gbif")
+
+order_df <- lapply(gbif_class, function(x) {
+  if (!is.data.frame(x)) return(NA)
+  order_row <- x[x$rank == "order", ]
+  if (nrow(order_row) > 0) return(order_row$name) else return(NA)
+}) %>% unlist()
+
+taxon_to_order <- data.frame(
+  original_name = taxa,
+  order = order_df,
+  stringsAsFactors = FALSE
+)
+
+extract_rank <- function(class_list, rank_name) {
+  sapply(class_list, function(x) {
+    if (!is.data.frame(x)) return(NA)
+    row <- x[x$rank == rank_name, ]
+    if (nrow(row) > 0) return(row$name) else return(NA)
+  })
+}
+
+# get each rank
+genus_df  <- extract_rank(gbif_class, "genus")
+family_df <- extract_rank(gbif_class, "family")
+order_df  <- extract_rank(gbif_class, "order")
+class_df  <- extract_rank(gbif_class, "class")
+
+# combine into one data frame
+taxon_lookup <- data.frame(
+  original_name = taxa,
+  genus = genus_df,
+  family = family_df,
+  order = order_df,
+  class = class_df,
+  stringsAsFactors = FALSE
+)
+
+# patch known missing values
+taxon_lookup$order[which(taxon_lookup$original_name == "Calanus")] <- "Calanoida"
+taxon_lookup$order[which(taxon_lookup$original_name == "Temora longicornis")] <- "Calanoida"
+taxon_lookup$order[which(taxon_lookup$original_name == "Hyperia")] <- "Amphipoda"
+taxon_lookup$order[which(taxon_lookup$original_name == "Thysanopoda")] <- "Euphausiacea"
+taxon_lookup$order[which(taxon_lookup$original_name == "Gammarus")] <- "Amphipoda"
+taxon_lookup$order[taxon_lookup$original_name == "Chaetognatha"] <- "Chaetognatha" #phylum
+taxon_lookup$order[taxon_lookup$original_name == "Ostracoda"] <- "Ostracoda" #class
+taxon_lookup$order[taxon_lookup$original_name == "Cladocera"] <- "Cladocera" #Diplostraca is the accepted name
+taxon_lookup$order[taxon_lookup$original_name == "Ammodytes"] <- "Ammodytes" # order Perciformes
+taxon_lookup$order[taxon_lookup$original_name == "Polychaeta"] <- "Polychaeta"
+taxon_lookup$order[taxon_lookup$original_name == "Isopoda"] <- "Isopoda"
+taxon_lookup$order[taxon_lookup$original_name == "Nematoscelis"] <- "Euphausiacea"
+taxon_lookup$order[taxon_lookup$original_name == "Appendicularia"] <- "Appendicularia"
+taxon_lookup$order[taxon_lookup$original_name == "Copepoda"] <- "Copepoda"
+taxon_lookup$order[taxon_lookup$original_name == "Cirripedia"] <- "Cirripedia"
+taxon_lookup$order[taxon_lookup$original_name == "Decopoda"] <- "Decapoda"
+taxon_lookup$order[taxon_lookup$original_name == "Gnathostomata"] <- "Gnathostomata"
+taxon_lookup$order[taxon_lookup$original_name == "Bivalvia"] <- "Bivalvia"  
+taxon_lookup$order[taxon_lookup$original_name == "Themisto"] <- "Amphipoda"
+taxon_lookup$order[taxon_lookup$original_name == "Temora"] <- "Calanoida"
+taxon_lookup$order[which(taxon_lookup$original_name == "Clione")] <- "Pteropoda"
+taxon_lookup$order[which(taxon_lookup$original_name == "Oithona")] <- "Cyclopoida"
+taxon_lookup$order[which(taxon_lookup$original_name == "Caridea")] <- "Decapoda"
+taxon_lookup$order[taxon_lookup$original_name == "Gastropoda"] <- "Gastropoda"
+taxon_lookup$order[taxon_lookup$original_name == "Limacina"] <- "Pteropoda"
+taxon_lookup$order[taxon_lookup$original_name == "Thaliacea"] <- "Thaliacea"
+taxon_lookup$order[taxon_lookup$original_name == "Tortanus discaudatus"] <- "Calanoida"
+taxon_lookup$order[taxon_lookup$original_name == "Foraminiferida"] <- "Foraminiferida" 
+taxon_lookup$order[taxon_lookup$original_name == "Eucalanus"] <- "Calanoida"
+taxon_lookup$order[taxon_lookup$original_name == "Coleoidea"] <- "Coleoidea"
+
+
+diets_with_order <- clean_diets %>%
+  left_join(taxon_lookup, 
+            by = c("scientificName_preyTaxon" = "original_name")) %>%
+  filter(!is.na(order)) 
+
+presence_by_order <- diets_with_order %>%
+  distinct(stomach_id, order) %>%
+  mutate(present = 1) %>%
+  pivot_wider(names_from = order, values_from = present, values_fill = 0) %>%
+  column_to_rownames("stomach_id")
+
+accum_curve <- specaccum(presence_by_order, method = "random")
+
+# Plot all fish separately
+accum_list <- list()
+par(mfrow = c(2, 3)) 
+for (sp in unique_species) {
+  # Subset and join order-level taxonomy
+  sub_df <- clean_diets %>%
+    filter(FishSpecies == sp) %>%
+    left_join(taxon_lookup, by = c("scientificName_preyTaxon" = "original_name")) %>%
+    filter(!is.na(order)) %>%
+    distinct(stomach_id, order) %>%
+    mutate(present = 1) %>%
+    pivot_wider(names_from = order, values_from = present, values_fill = 0) %>%
+    column_to_rownames("stomach_id")
+  
+  # Only plot if >1 stomach
+  if (nrow(sub_df) > 1) {
+    acc <- specaccum(sub_df, method = "random")
+    accum_list[[sp]] <- acc
+    plot(acc, ci.type = "polygon", col = "black", lwd = 2,
+         ci.lty = 0, ci.col = rgb(0, 0, 0, 0.2),
+         main = sp, xlab = "Number of stomachs", ylab = "Prey orders")
+  }
+}
+
+## adding vertical line showing 95%
+estimate_plateau <- function(accum_obj, threshold = 0.95) {
+  max_rich <- max(accum_obj$richness)
+  plateau_level <- max_rich * threshold
+  idx <- which(accum_obj$richness >= plateau_level)[1]
+  
+  tibble(
+    stomachs_needed = accum_obj$sites[idx],
+    richness_reached = accum_obj$richness[idx],
+    max_richness = max_rich
+  )
+}
+
+# Get info for each species (based on prey order)
+plateau_order_df <- map_dfr(accum_list, estimate_plateau, .id = "FishSpecies")
+
+# Build long-format df with CI
+accum_order_df <- map2_dfr(
+  .x = accum_list[plateau_order_df$FishSpecies],
+  .y = names(accum_list[plateau_order_df$FishSpecies]),
+  ~ tibble(
+    FishSpecies = .y,
+    stomachs = .x$sites,
+    richness = .x$richness,
+    sd = .x$sd,
+    lower = .x$richness - .x$sd,
+    upper = .x$richness + .x$sd
+  )
+)
+
+# Join to add stomach threshold
+accum_order_df <- left_join(accum_order_df, plateau_order_df, by = "FishSpecies")
+
+common_names <- c(
+  C_har = "Atlantic Herring",
+  P_tri = "Atlantic Mackerel",
+  S_sco = "Butterfish",
+  A_aes = "Blueback Herring",
+  A_pse = "Alewife"
+)
+
+accum_order_df <- accum_order_df %>%
+  filter(FishSpecies %in% names(common_names)) %>%
+  mutate(FishCommon = common_names[FishSpecies])
+
+text_labels <- distinct(accum_order_df, FishSpecies, FishCommon, stomachs_needed)
+
+
+ggplot(accum_order_df, aes(x = stomachs, y = richness)) +
+  geom_ribbon(aes(ymin = lower, ymax = upper), 
+              fill = "gray80", alpha = 0.5) +
+  geom_line(color = "black", linewidth = 1) +
+  facet_wrap(~FishCommon, scales = "free_x") +
+  geom_vline(aes(xintercept = stomachs_needed), 
+             linetype = "dashed", color = "blue") +
+  geom_text(data = text_labels,
+            aes(x = stomachs_needed, y = Inf, 
+                label = paste0(stomachs_needed, " stomachs")),
+            vjust = 1.5, hjust = 1.3, color = "black", size = 3.9) +
+  labs(x = "Number of stomachs", y = "Cumulative prey orders",
+       title = "Species Accumulation Curves (Prey Orders)") +
+  theme_minimal(base_size = 12) +
+  theme(strip.text = element_text(face = "bold", size = 14),
+        axis.text = element_text(color = "black", size = 10),
+        axis.title = element_text(size = 14))
